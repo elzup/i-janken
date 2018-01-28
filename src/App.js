4c2f28760663508e99ea34679b232784568efd88
elzup <guild0105@gmail.com>
2018-01-28 18:31:21 +0900

    Number changable

--- src/App.js
+++ src/App.js
@@ -3,31 +3,61 @@
 import React from "react";
 import Board from "./Board";
 import _ from "lodash";
+import Button from "material-ui/Button";
 
-const labels = "ABCDEFG".split("");
+const NUM_MAX = 9;
+
 type State = {
   labels: Array<any>,
-  open: boolean
+  num: number
 };
 
+function makeLabels(n) {
+  return _.range(1, n + 1);
+}
+
 class App extends React.Component<{}, State> {
-  state = { labels, open: false };
+  state = { labels: makeLabels(3), num: 3 };
+  setNum = num => {
+    this.setState({
+      num,
+      labels: makeLabels(num)
+    });
+  };
   render() {
     const { state } = this;
+    console.log(state);
     return (
       <div>
         <div>Graph page</div>
         <div>
           <Board labels={state.labels} />
         </div>
-        <button
-          onClick={() => {
-            console.log(state.labels);
-            this.setState({ labels: _.shuffle(state.labels) });
-          }}
-        >
-          Change
-        </button>
+        <div style={{ display: "flex" }}>
+          {_.range(2, NUM_MAX).map(i => (
+            <Button
+              key={i}
+              raised
+              onClick={() => {
+                this.setNum(i);
+              }}
+            >
+              {i}
+            </Button>
+          ))}
+        </div>
+        <div>
+          <Button
+            raised
+            color="primary"
+            onClick={() => {
+              this.setState({ labels: _.shuffle(state.labels) });
+            }}
+            style={{ width: "100%" }}
+          >
+            Pon!
+          </Button>
+        </div>
       </div>
     );
   }
